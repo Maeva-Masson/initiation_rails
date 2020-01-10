e5bb4ccefb0c1f23d3a80de8a3663ad78b15e818
Maeva Masson <maeva@MacBook-Pro-de-laetitia.local>
2020-01-10 13:00:32 +0100

    Simplification des routes via resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy'
+  resources :books, only: [:index, :create, :show, :update, :destroy]
 end
