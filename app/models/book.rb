610acc87d2297f62497b944baeb7c3b11dc9e860
Maeva Masson <maeva@MacBook-Pro-de-laetitia.local>
2020-01-10 12:50:23 +0100

    Ajout des validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,12 @@
 class Book < ActiveRecord::Base
   belongs_to :category
+
+  validates :title, presence:
+  { message: 'Le titre doit être renseigné.'}
+
+  validates :title, uniqueness:
+  { message: "Ce titre est déjà pris."}
+
+  scope :french, -> { where(category_id: 1) }
+  scope :monde, -> { where(category_id: 2)}
 end
